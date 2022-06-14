bc882efba90d7dbb9cf76d7294d50c0bbb6c4e9f
Franciscopuga097 <107226812+Franciscopuga097@users.noreply.github.com>
2022-06-13 19:25:45 -0500

    Add files via upload

---
+++ 1.1/inc/navbar.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span> 
+            </button>
+            <a class="navbar-brand" href="index.php"></i>&nbsp;&nbsp;PUGATECH</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class=" nav navbar-nav navbar-right">
+                <li>
+                    <a href="./index.php"><span class="glyphicon glyphicon-home"></span> &nbsp; Inicio</a>
+                </li>
+                <?php if(!isset($_SESSION['tipo']) && !isset($_SESSION['nombre'])): ?>
+                <li>
+                    <a href="./index.php?view=registro"><i class="fa fa-users"></i>&nbsp;&nbsp;Registro</a>
+                </li>
+                <li>
+                    <a href="#!" data-toggle="modal" data-target="#modalLog"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;Login</a>
+                </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="modalLog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h4 class="modal-title text-center text-primary" id="myModalLabel">Bienvenido a PUGATECH</h4>
+            </div>
+      </div>
+    </div>
+</div>
